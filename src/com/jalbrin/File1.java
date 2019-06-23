4e1de0500cdbaa173b29b29ec909e9c0abbd07db
sanjay rally <sanjayrally@yahoo.com>
2019-06-23 14:04:37 -0400

    Updated main methods in File1 and File2

--- src/com/jalbrin/File1.java
+++ src/com/jalbrin/File1.java
@@ -3,7 +3,7 @@ package com.jalbrin;
 public class File1 {
 	
 	public static void main(String[] args) {
-		
+		System.out.println("Testing File1");
 	}
 
 }
--- src/com/jalbrin/File2.java
+++ src/com/jalbrin/File2.java
@@ -2,6 +2,6 @@ package com.jalbrin;
 
 public class File2 {
 	public static void main(String[] args) {
-		
+		System.out.println("Testing File2");
 	}
 }
